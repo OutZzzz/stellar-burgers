116caa6c49e6169f81a5346691d238960c19f487
OutZzzz <outzz@yandex.ru>
2026-02-17 19:37:13 +0300

    feat: добавил маршруты

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,49 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
+import { Routes, Route } from 'react-router-dom';
 import { AppHeader } from '@components';
+import { ProtectedRoute } from '../protected-route';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login' element={<ProtectedRoute children={<Login />} />} />
+      <Route
+        path='/register'
+        element={<ProtectedRoute children={<Register />} />}
+      />
+      <Route
+        path='/forgot-password'
+        element={<ProtectedRoute children={<ForgotPassword />} />}
+      />
+      <Route
+        path='/reset-password'
+        element={<ProtectedRoute children={<ResetPassword />} />}
+      />
+      <Route
+        path='/profile'
+        element={<ProtectedRoute children={<Profile />} />}
+      />
+      <Route
+        path='/profile/orders'
+        element={<ProtectedRoute children={<ProfileOrders />} />}
+      />
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
   </div>
 );
 
