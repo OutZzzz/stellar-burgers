9f9130bc7ee6475d14fe19703f1f172ead9ba9d1
OutZzzz <outzz@yandex.ru>
2026-02-19 21:41:49 +0300

    feat: вывел на страницу ингредиенты

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,14 +1,31 @@
 import { useState, useRef, useEffect, FC } from 'react';
 import { useInView } from 'react-intersection-observer';
 
-import { TTabMode } from '@utils-types';
+import { TIngredient, TTabMode } from '@utils-types';
 import { BurgerIngredientsUI } from '../ui/burger-ingredients';
+import { useSelector } from '../../services/store';
 
 export const BurgerIngredients: FC = () => {
   /** TODO: взять переменные из стора */
-  const buns = [];
-  const mains = [];
-  const sauces = [];
+  const { data, isLoading, error } = useSelector((state) => state.ingredients);
+
+  const buns: TIngredient[] = [];
+  const mains: TIngredient[] = [];
+  const sauces: TIngredient[] = [];
+
+  data?.forEach((item) => {
+    switch (item.type) {
+      case 'bun':
+        buns.push(item);
+        break;
+      case 'main':
+        mains.push(item);
+        break;
+      case 'sauces':
+        sauces.push(item);
+        break;
+    }
+  });
 
   const [currentTab, setCurrentTab] = useState<TTabMode>('bun');
   const titleBunRef = useRef<HTMLHeadingElement>(null);
@@ -47,7 +64,7 @@ export const BurgerIngredients: FC = () => {
       titleSaucesRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
-  return null;
+  /* return null; */
 
   return (
     <BurgerIngredientsUI
