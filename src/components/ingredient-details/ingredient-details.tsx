2c630ca2a0ec735228a2c81dc7249d9e114d0b33
OutZzzz <outzz@yandex.ru>
2026-02-20 16:32:15 +0300

    Feat: добавил получение ингредиента для модального окна

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useSelector } from '../../services/store';
+import { useParams } from 'react-router-dom';
 
 export const IngredientDetails: FC = () => {
   /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { data, isLoading } = useSelector((state) => state.ingredients);
+  const { id } = useParams();
+
+  /*  const ingredientData = null; */
+  const ingredientData = data?.find((item) => item._id === id);
+
+  if (isLoading) {
+    return <Preloader />;
+  }
 
   if (!ingredientData) {
     return <Preloader />;
