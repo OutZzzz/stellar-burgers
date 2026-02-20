bad711afcb8a026d32207e7efbd1978794f1f3d3
OutZzzz <outzz@yandex.ru>
2026-02-20 16:33:27 +0300

    feat: добавлена функция закрыытия модального окна

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -11,60 +11,77 @@ import {
 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 import { ProtectedRoute } from '../protected-route';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <Routes>
-      <Route path='/' element={<ConstructorPage />} />
-      <Route path='/feed' element={<Feed />} />
-      <Route path='/login' element={<ProtectedRoute children={<Login />} />} />
-      <Route
-        path='/register'
-        element={<ProtectedRoute children={<Register />} />}
-      />
-      <Route
-        path='/forgot-password'
-        element={<ProtectedRoute children={<ForgotPassword />} />}
-      />
-      <Route
-        path='/reset-password'
-        element={<ProtectedRoute children={<ResetPassword />} />}
-      />
-      <Route
-        path='/profile'
-        element={<ProtectedRoute children={<Profile />} />}
-      />
-      <Route
-        path='/profile/orders'
-        element={<ProtectedRoute children={<ProfileOrders />} />}
-      />
-      <Route path='*' element={<NotFound404 />} />
-      <Route
-        path='/feed/:number'
-        element={<Modal title='' children={<OrderInfo />} onClose={() => {}} />}
-      />
-      <Route
-        path='/ingredients/:id'
-        element={
-          <Modal title='' children={<IngredientDetails />} onClose={() => {}} />
-        }
-      />
-      <Route
-        path='/profile/orders/:number'
-        element={
-          <ProtectedRoute
-            children={
-              <Modal title='' children={<OrderInfo />} onClose={() => {}} />
-            }
-          />
-        }
-      />
-    </Routes>
-  </div>
-);
+const App = () => {
+  const navigate = useNavigate();
+
+  const onClose = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route
+          path='/login'
+          element={<ProtectedRoute children={<Login />} />}
+        />
+        <Route
+          path='/register'
+          element={<ProtectedRoute children={<Register />} />}
+        />
+        <Route
+          path='/forgot-password'
+          element={<ProtectedRoute children={<ForgotPassword />} />}
+        />
+        <Route
+          path='/reset-password'
+          element={<ProtectedRoute children={<ResetPassword />} />}
+        />
+        <Route
+          path='/profile'
+          element={<ProtectedRoute children={<Profile />} />}
+        />
+        <Route
+          path='/profile/orders'
+          element={<ProtectedRoute children={<ProfileOrders />} />}
+        />
+        <Route path='*' element={<NotFound404 />} />
+        <Route
+          path='/feed/:number'
+          element={
+            <Modal title='' children={<OrderInfo />} onClose={() => {}} />
+          }
+        />
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal
+              title='Детали ингредиента'
+              children={<IngredientDetails />}
+              onClose={onClose}
+            />
+          }
+        />
+        <Route
+          path='/profile/orders/:number'
+          element={
+            <ProtectedRoute
+              children={
+                <Modal title='' children={<OrderInfo />} onClose={() => {}} />
+              }
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
