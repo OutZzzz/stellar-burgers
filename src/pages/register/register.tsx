63df7d405391231e7cf98efef3bbbc7ab56983c2
OutZzzz <outzz@yandex.ru>
2026-02-22 21:03:22 +0300

    feat: добавил сабмит эвент для регистрации и вывел ошибки

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,29 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { AppDispatch, useDispatch, useSelector } from '../../services/store';
+import { registerUser, loginErrorSelector } from '../../features/userSlice';
+import { TRegisterData } from '@api';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch: AppDispatch = useDispatch();
+  const loginUserError = useSelector(loginErrorSelector);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    const userData: TRegisterData = {
+      email: email,
+      name: userName,
+      password: password
+    };
+    dispatch(registerUser(userData));
   };
 
   return (
     <RegisterUI
-      errorText=''
+      errorText={loginUserError ?? ''}
       email={email}
       userName={userName}
       password={password}
