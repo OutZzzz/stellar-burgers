49c705f59add79eaa6240d9e3f828ea9bc87315d
OutZzzz <outzz@yandex.ru>
2026-02-23 14:39:51 +0300

    fix

---
+++ src/features/index.ts
@@ -0,0 +1,4 @@
+export { constructorSlice } from './constructorSlice';
+export { ingredientsSlice } from './ingredientsSlice';
+export { userSlice } from './userSlice';
+export { orderSlice } from './orderSlice';
